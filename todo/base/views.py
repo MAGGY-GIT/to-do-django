67b16bf8c96d6b412b465ca3135add10b760a8c7
MAGGY-GIT <kairumaggie29@gmail.com>
2024-08-11 00:36:19 +0300

    created a detail view class that will show more information on a task on the list with its respective html file

--- todo/base/views.py
+++ todo/base/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+#this will allow the views to show more information on the tasks in the models
 from .models import Task
 # Create your views here.
 
@@ -8,4 +10,10 @@ from .models import Task
 class TaskList(ListView):
      #I don't have to capitalize the class names always
      #we are inheriting from ListView as well as all ots functionalities
+     model = Task
+     context_object_name = "tasks"
+     #this will create a context dictionary name rather than using the standard object_list
+
+
+class TaskDetail(DetailView):
      model = Task
